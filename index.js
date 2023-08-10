2975e96ce5f5c9edc6533f04ee95fa4fdb272cc3
razibhasan1 <razib1500@diu.edu.bd>
2023-08-10 23:12:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3100
 
-app.get('/', (req, res) => res.send('Simple APP with Terraform!'))
+app.get('/', (req, res) => res.send('Hello Its Working!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
